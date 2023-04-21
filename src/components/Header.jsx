24886de115466b6a4ae7988742f582bd0573123c
Gian Mendoza <mendoza.gian@hotmail.com>
2023-04-20 21:21:58 -0300

    v2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 export const Header = () => {
     return (
         <h1 className='font-black text-5xl text-center md:w-2/3 mx-auto'>
-            Lambesuqueros {' '}
-            <span className='text-indigo-600'>Veterinaria</span>
+            Veterinaria {' '}
+            <span className='text-indigo-600'>Lambesuqueros</span>
         </h1>
     )
 }

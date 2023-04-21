07c90ce9192d719db3ab0cccd91daa6f9e942942
Gian Mendoza <mendoza.gian@hotmail.com>
2023-04-21 15:02:04 -0300

    Update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export const Header = () => {
     return (
         <h1 className='font-black text-5xl text-center md:w-2/3 mx-auto'>
             Veterinaria {' '}
-            <span className='text-indigo-600'>MDP</span>
+            <span className='text-indigo-600'>Gian Mendoza</span>
         </h1>
     )
 }

4c3d5574d2d379effa21be6a6f2cc65e1f52b857
Gian Mendoza <mendoza.gian@hotmail.com>
2023-04-21 14:57:54 -0300

    cambio de muestra

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export const Header = () => {
     return (
         <h1 className='font-black text-5xl text-center md:w-2/3 mx-auto'>
             Veterinaria {' '}
-            <span className='text-indigo-600'>Lambesuqueros</span>
+            <span className='text-indigo-600'>MDP</span>
         </h1>
     )
 }
